5ee574d491bb0f58fd8c425a6d97d1466d2d6ab9
smarquie <38230206+smarquie@users.noreply.github.com>
2025-07-14 12:44:15 -0700

    Update trading_engine.py

--- trading_engine.py
+++ trading_engine.py
@@ -49,9 +49,9 @@ class TradingEngine:
     def __init__(self):
         self.config = BotConfig
         self.exchange = ExchangeInterface(mode=BotConfig.MODE)
-        self.portfolio = self.exchange.get_portfolio()
-        self.aggregator = SignalAggregator()
+        self.portfolio = Portfolio()
         self.telegram = TelegramNotifier(BotConfig.TELEGRAM_BOT_TOKEN, BotConfig.TELEGRAM_CHAT_ID)
+        self.aggregator = SignalAggregator()
         self.is_running = False
         self.cycle_count = 0
         self.symbols = BotConfig.TRADING_SYMBOLS
