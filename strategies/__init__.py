4987178b419ba479268f619960718870d13864c2
smarquie <38230206+smarquie@users.noreply.github.com>
2025-07-14 21:04:37 -0700

    Update __init__.py

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,11 +1,13 @@
 # strategies/__init__.py
 
+from .signal_aggregator import SignalAggregator
 from .ultra_scalp import UltraScalpStrategy
 from .fast_scalp import FastScalpStrategy
 from .quick_momentum import QuickMomentumStrategy
 from .ttm_squeeze import TTMSqueezeStrategy
 
 __all__ = [
+    "SignalAggregator",
     "UltraScalpStrategy",
     "FastScalpStrategy",
     "QuickMomentumStrategy",
